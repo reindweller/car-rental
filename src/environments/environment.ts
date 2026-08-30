a754ac6c494ce1d47dcfa2d7764e469b2dc2c87f
reindweller <22352689+reindweller@users.noreply.github.com>
2026-08-31 04:37:58 +0800

    Configure frontend for new AWS backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
   aws: {
-    region: 'ap-southeast-1',
-    userPoolId: 'ap-southeast-1_SmDpRfEji',
-    userPoolClientId: '3f3tfcc87slngdgo17q9n9i60o',
-    apiUrl: 'https://j2briozmp5.execute-api.ap-southeast-1.amazonaws.com/v1',
+    region: 'us-east-2',
+    userPoolId: 'us-east-2_6TPzRxwP1',
+    userPoolClientId: '5f0mgkuu3ra6hqoku73cltjrip',
+    apiUrl: 'https://goq03acjtc.execute-api.us-east-2.amazonaws.com/v1',
   },
   stripe: {
     publishableKey: 'pk_test_51U3dViBUs7hYlMO2OnPPlS7KNnvNMifYothrqOGckkgzklEHuwgQ0pktGUSLi2X9nCc9N2gkGyFVIzh861uTHrJx00IdHGQltX',
